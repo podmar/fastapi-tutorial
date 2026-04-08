36335b4c28f5d5005e87a4a840259d82a741f14a
podmar <marta.podziewska@gmail.com>
2026-04-08 12:44:54 +0200

    create an Enum as a parameter type

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+
+class AnimalName(str, Enum):
+    snake = "snake"
+    frog = "frog"
+    sheep = "sheep"
+
 
 app = FastAPI()
 
@@ -16,3 +25,13 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: int):
     return {"user_id": user_id}
+
+
+@app.get("/animals/{animal_name}")
+async def get_animal(animal_name: AnimalName):
+    if animal_name is AnimalName.frog:
+        return {"animal_name": animal_name, "message": "kum, kum"}
+    if animal_name is AnimalName.snake:
+        return {"animal_name": animal_name, "message": "ssssssss..."}
+
+    return {"animal_name": animal_name, "message": "meeeeee..."}
