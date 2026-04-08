4d18215dacfe957cd493de15ab9cbbce1b799e40
podmar <marta.podziewska@gmail.com>
2026-04-08 22:49:17 +0200

    add an optional query parameter

--- main.py
+++ main.py
@@ -47,3 +47,10 @@ async def read_file(file_path: str):
 @app.get("/items")
 async def read_items(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"intem_id": item_id, "q": q}
+    return {"item_id": item_id}
