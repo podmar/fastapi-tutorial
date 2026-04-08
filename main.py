f606616a61b3f2be6b697a3f6e364dc57907cd13
podmar <marta.podziewska@gmail.com>
2026-04-08 12:25:05 +0200

    add a predefined current user path

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "current user"}
+
+
 @app.get("/users/{user_id}")
 async def read_user(user_id: int):
     return {"user_id": user_id}
