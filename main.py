433d01613ca8d92de3317d899db043b4498b1c9a
podmar <marta.podziewska@gmail.com>
2026-04-08 22:36:16 +0200

    create read item function for query parameters

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ class AnimalName(str, Enum):
     sheep = "sheep"
 
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 app = FastAPI()
 
 
@@ -40,3 +42,8 @@ async def get_animal(animal_name: AnimalName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+
+@app.get("/items")
+async def read_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
